48832f3c552b1db538c655421ddf97f5d059e09b
Evlusha <106895767+Evlusha@users.noreply.github.com>
2024-05-12 21:57:55 +0800

    Update zd1.py

--- zd1.py
+++ zd1.py
@@ -1,21 +1,23 @@
+
 import numpy as np
 import math
 
 def pudge(start, end, step):
     for x in np.arange(start, end+step, step):
-        if -10 <= x <= -6:
-            result = round((-1*(math.sqrt(4-(math.pow(x+8, 2))))+2)*100) / 100
-            print(x, "\t", result)
-        elif -6 < x <= -4:
-            result = 2
-            print(x, "\t", result)
-        elif -4 < x <= 2:
+        if x >= -4 and x <= -2:
+            result = x + 3
+            print(x, "\t", result) 
+        elif x > -2 and x <= 4:
             result = -0.5 * x
-            if result == -0:
+            if result == 0:
                 result = abs(result)
             print(x, "\t", result)
-        elif x > 2:
-            result = x - 3
+        elif x > 4 and x <= 6:
+            result = -2
+            print(x, "\t", result)
+       
+        elif x >= 6 and x <= 10:
+            result = round((1 * (math.sqrt(4 - (pow(x - 8, 2)))) - 2) * 100) / 100
             print(x, "\t", result)
 
 
@@ -24,3 +26,4 @@ xend = int(input("Введите конечный x: "))
 step = float(input("Введите шаг: "))
 print("X\tY")
 pudge(xstart, xend, step)
+
